32df67219ca355080486527a139ecd1a87f008a2
Ahmed Melouk <forprogramming8@gmail.com>
2026-04-24 21:13:36 +0300

    feat: implement JWT token revocation strategy using JwtDenylist model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_24_180056) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_24_180138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "jwt_denylists", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_denylists_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
